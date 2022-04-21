1f1ab2dd9cc11c4dda51eeee18c7082fee4421b7
Khalfan1998 <khalfan.ikns@gmail.com>
2022-04-21 14:30:08 +0300

    adding restaurant items home page

--- BahrainEatsUser/App.js
+++ BahrainEatsUser/App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
+      {/* Restaurant item */}
+      <View style={styles.restaurantContainer}>
+        <Image
+          source={{
+            uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg",
+          }}
+          style={styles.image}
+        />
+        <Text style={styles.title}>El Cabo Coffe Bar Tres De Mayo</Text>
+        <Text style={styles.subtitle}>BHD 1 15-25 minutes</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +27,22 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    padding: 10,
+  },
+  restaurantContainer: {
+    width: "100%",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 5 / 3,
+    marginBottom: 5,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 5,
+  },
+  subtitle: {
+    color: "grey",
   },
 });

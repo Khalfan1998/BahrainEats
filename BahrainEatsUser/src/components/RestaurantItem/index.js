8915effd7029163d880e4804effd02ef2a256c6a
Khalfan1998 <khalfan.ikns@gmail.com>
2022-04-28 11:12:28 +0300

    adding flat list restaurants page

--- BahrainEatsUser/src/components/RestaurantItem/index.js
+++ BahrainEatsUser/src/components/RestaurantItem/index.js
@@ -9,11 +9,18 @@ const RestaurantItem = ({ restaurant }) => {
         }}
         style={styles.image}
       />
-      <Text style={styles.title}>{restaurant.name}</Text>
-      <Text style={styles.subtitle}>
-        BHD {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
-        {restaurant.maxDeliveryTime} minutes
-      </Text>
+      <View style={styles.row}>
+        <View>
+          <Text style={styles.title}>{restaurant.name}</Text>
+          <Text style={styles.subtitle}>
+            BHD {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
+            {restaurant.maxDeliveryTime} minutes
+          </Text>
+        </View>
+        <View style={styles.rating}>
+          <Text>{restaurant.rating}</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -32,10 +39,23 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 16,
-    fontWeight: "500",
+    fontWeight: "bold",
     marginVertical: 5,
   },
   subtitle: {
     color: "grey",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  rating: {
+    marginLeft: "auto",
+    backgroundColor: "lightgrey",
+    width: 30,
+    height: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
   },
 });

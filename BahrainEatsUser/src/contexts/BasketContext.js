17fa46afb1c9b0406188abd3169d9e5524a6be20
Khalfan1998 <khalfan.ikns@gmail.com>
2022-05-12 15:39:03 +0300

    adding basket

--- BahrainEatsUser/src/contexts/BasketContext.js
+++ BahrainEatsUser/src/contexts/BasketContext.js
@@ -1,10 +1,45 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import { DataStore } from "aws-amplify";
 import { Basket, BasketDish } from "../models";
+import { useAuthContext } from "./AuthContext";
 
 const BasketContext = createContext({});
+
 const BasketContextProvider = ({ children }) => {
-  return <BasketContext.Provider>{children}</BasketContext.Provider>;
+  const { dbUser } = useAuthContext();
+
+  const [restaurant, setRestaurant] = useState(null);
+  const [basket, setBasket] = useState(null);
+
+  useEffect(() => {
+    DataStore.query(Basket, (b) =>
+      b.restaurantID("eq", restaurant).userID("eq", dbUser)
+    ).then((baskets) => setBasket(baskets[0]));
+  }, [dbUser, restaurant]);
+
+  const addDishToBasket = async (dish, quantity) => {
+    //get the existing basket or create a new one
+    let theBasket = basket || (await createNewBasket());
+
+    //create a BasketDish item and save to Datastore
+    DataStore.save(
+      new BasketDish({ quantity, Dish: dish, basketID: theBasket.id })
+    );
+  };
+
+  const createNewBasket = async () => {
+    const newBasket = await DataStore.save(
+      new Basket({ userID: dbUser.id, restaurantID: restaurant.id })
+    );
+    setBasket(newBasket);
+    return newBasket;
+  };
+
+  return (
+    <BasketContext.Provider value={{ addDishToBasket, setRestaurant }}>
+      {children}
+    </BasketContext.Provider>
+  );
 };
 
 export default BasketContextProvider;
--- BahrainEatsUser/src/screens/RestaurantDetailsScreen/index.js
+++ BahrainEatsUser/src/screens/RestaurantDetailsScreen/index.js
@@ -14,6 +14,7 @@ import styles from "./styles";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { DataStore } from "aws-amplify";
 import { Restaurant, Dish } from "../../models";
+import { useBasketContext } from "../../contexts/BasketContext";
 
 const RestaurantDetailsScreen = () => {
   const [restaurant, setRestaurant] = useState(null);
@@ -24,10 +25,13 @@ const RestaurantDetailsScreen = () => {
 
   const id = route.params?.id;
 
+  const { setRestaurant: setBasketRestaurant } = useBasketContext();
+
   useEffect(() => {
     if (!id) {
       return;
     }
+    setBasketRestaurant(null);
     //fetch the restaurant with the id
     DataStore.query(Restaurant, id).then(setRestaurant);
 
@@ -35,6 +39,10 @@ const RestaurantDetailsScreen = () => {
       setDishes
     );
   }, [id]);
+
+  useEffect(() => {
+    setBasketRestaurant(restaurant);
+  }, [restaurant]);
 
   if (!restaurant) {
     return <ActivityIndicator size="large" color="gray" />;

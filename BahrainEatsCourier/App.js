a3bdf7c83c8f157f42aa5d6ff5dacc09ed3709dc
Khalfan1998 <khalfan.ikns@gmail.com>
2022-05-16 14:44:21 +0300

    adding order item list for driver app

--- BahrainEatsCourier/App.js
+++ BahrainEatsCourier/App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import orders from "./assets/data/orders.json";
+import OrderItem from "./src/components/OrderItem";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <OrderItem />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
 });

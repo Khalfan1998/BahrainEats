9905d8e1f5a8c82a6d3cb110407ce3a19e36dae6
Khalfan1998 <khalfan.ikns@gmail.com>
2022-05-23 11:06:11 +0300

    pulling backend from aws amplify

--- BahrainEatsCourier/App.js
+++ BahrainEatsCourier/App.js
@@ -2,6 +2,15 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./src/navigation";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./src/aws-exports";
+
+Amplify.configure({
+  ...awsconfig,
+  Analytics: {
+    disabled: true,
+  },
+});
 
 export default function App() {
   return (

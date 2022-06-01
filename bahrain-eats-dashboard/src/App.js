a52fd1da53d70c65c854c221bf0702c8db574824
Khalfan1998 <khalfan.ikns@gmail.com>
2022-06-01 11:43:26 +0300

    adding react router

--- bahrain-eats-dashboard/src/App.js
+++ bahrain-eats-dashboard/src/App.js
@@ -1,12 +1,13 @@
 import DetailedOrder from "./screens/DetailedOrder";
 import Orders from "./screens/Orders";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      {/* <DetailedOrder /> */}
-      <Orders />
-    </>
+    <Routes>
+      <Route path="" element={<Orders />} />
+      <Route path="order/:id" element={<DetailedOrder />} />
+    </Routes>
   );
 }
 

a33468c89bf780957a65ee4f43ef48ce268f3220
Khalfan1998 <khalfan.ikns@gmail.com>
2022-06-01 13:43:45 +0300

    adding sideMenu component

--- bahrain-eats-dashboard/src/App.js
+++ bahrain-eats-dashboard/src/App.js
@@ -1,13 +1,33 @@
 import DetailedOrder from "./screens/DetailedOrder";
 import Orders from "./screens/Orders";
 import { Routes, Route } from "react-router-dom";
+import { Layout, Image } from "antd";
+import SideMenu from "../src/components/SideMenu";
+
+const { Sider, Content, Footer } = Layout;
 
 function App() {
   return (
-    <Routes>
-      <Route path="" element={<Orders />} />
-      <Route path="order/:id" element={<DetailedOrder />} />
-    </Routes>
+    <Layout>
+      <Sider style={{ height: "100vh", backgroundColor: "white" }}>
+        <Image
+          src="https://previews.123rf.com/images/jianghaistudio/jianghaistudio1701/jianghaistudio170100032/69541516-food-logo-made-from-the-flag-of-bahrain.jpg"
+          preview={false}
+        />
+        <SideMenu />
+      </Sider>
+      <Layout>
+        <Content>
+          <Routes>
+            <Route path="" element={<Orders />} />
+            <Route path="order/:id" element={<DetailedOrder />} />
+          </Routes>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          BahrainEats Restaurant Dashboard ©2022
+        </Footer>
+      </Layout>
+    </Layout>
   );
 }
 

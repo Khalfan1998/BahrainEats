a84fc58adb4ce57b6c6e29974c9f7d0b4b00d845
Khalfan1998 <khalfan.ikns@gmail.com>
2022-06-20 10:41:29 +0300

    dashboard sidebar icons

--- bahrain-eats-dashboard/src/components/SideMenu/index.js
+++ bahrain-eats-dashboard/src/components/SideMenu/index.js
@@ -2,6 +2,14 @@ import { Menu } from "antd";
 import { useNavigate } from "react-router-dom";
 import { Auth } from "aws-amplify";
 import { useRestaurantContext } from "../../contexts/RestaurantContext";
+import {
+  OrderedListOutlined,
+  FormOutlined,
+  HistoryOutlined,
+  SettingOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import "./index.css";
 
 const SideMenu = () => {
   const navigate = useNavigate();
@@ -29,14 +37,17 @@ const SideMenu = () => {
     {
       key: "/",
       label: "Orders",
+      icon: <OrderedListOutlined />,
     },
     {
       key: "menu",
       label: "Menu",
+      icon: <FormOutlined />,
     },
     {
       key: "order-history",
       label: "Order History",
+      icon: <HistoryOutlined />,
     },
   ];
 
@@ -45,17 +56,20 @@ const SideMenu = () => {
     {
       key: "settings",
       label: "Settings",
+      icon: <SettingOutlined />,
     },
     {
       key: "signOut",
       label: "Sign out",
       danger: "true",
+      icon: <LogoutOutlined />,
     },
   ];
 
   return (
     <>
-      {restaurant && <h2>{restaurant.name}</h2>}
+      {restaurant && <h2 className="restaurantName">{restaurant.name}</h2>}
+
       <Menu items={menuItems} onClick={onClick} />
     </>
   );
